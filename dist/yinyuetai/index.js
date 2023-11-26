d07af594edacb43dae7c23f0bab53981722d07ae
猫头猫 <lhx_xjtu@163.com>
2023-11-26 11:05:48 +0800

    feat: 音悦台

--- dist/yinyuetai/index.js
+++ dist/yinyuetai/index.js
@@ -7,9 +7,11 @@ function formatMusicItem(item) {
     return {
         id: item.id,
         title: item.title,
-        artist: (item === null || item === void 0 ? void 0 : item.allArtistNames) || ((_b = (_a = item.artists) === null || _a === void 0 ? void 0 : _a.map) === null || _b === void 0 ? void 0 : _b.call(_a, (s) => s.name).join(", ")) || ((_c = item.user) === null || _c === void 0 ? void 0 : _c.niceName),
+        artist: (item === null || item === void 0 ? void 0 : item.allArtistNames) ||
+            ((_b = (_a = item.artists) === null || _a === void 0 ? void 0 : _a.map) === null || _b === void 0 ? void 0 : _b.call(_a, (s) => s.name).join(", ")) ||
+            ((_c = item.user) === null || _c === void 0 ? void 0 : _c.niceName),
         artwork: item === null || item === void 0 ? void 0 : item.headImg,
-        urls: (_d = item === null || item === void 0 ? void 0 : item.fullClip) === null || _d === void 0 ? void 0 : _d.urls
+        urls: (_d = item === null || item === void 0 ? void 0 : item.fullClip) === null || _d === void 0 ? void 0 : _d.urls,
     };
 }
 function formatArtistItem(item) {
@@ -27,120 +29,67 @@ async function searchMusic(query, page) {
     }
     lastQuery = query;
     let data = JSON.stringify({
-        "searchType": "MV",
-        "key": query,
-        "sinceId": lastMusicId,
-        "size": pageNum,
-        "requestTagRows": [
+        searchType: "MV",
+        key: query,
+        sinceId: lastMusicId,
+        size: pageNum,
+        requestTagRows: [
             {
-                "key": "sortType",
-                "chosenTags": [
-                    "HOTTEST"
-                ]
+                key: "sortType",
+                chosenTags: ["HOTTEST"],
             },
             {
-                "key": "source",
-                "chosenTags": [
-                    "-1"
-                ]
+                key: "source",
+                chosenTags: ["-1"],
             },
             {
-                "key": "duration",
-                "chosenTags": [
-                    "-1"
-                ]
-            }
-        ]
+                key: "duration",
+                chosenTags: ["-1"],
+            },
+        ],
     });
     let config = {
-        method: 'post',
+        method: "post",
         maxBodyLength: Infinity,
-        url: 'https://search-api.yinyuetai.com/search/get_search_result.json',
+        url: "https://search-api.yinyuetai.com/search/get_search_result.json",
         headers: {
-            'referrer': 'https://www.yinyuetai.com/',
-            'accept': 'application/json',
-            'content-type': 'application/json',
-            'wua': 'YYT/1.0.0 (WEB;web;11;zh-CN;kADiV2jNJFy2ryvuyB5Ne)'
+            referrer: "https://www.yinyuetai.com/",
+            accept: "application/json",
+            "content-type": "application/json",
+            wua: "YYT/1.0.0 (WEB;web;11;zh-CN;kADiV2jNJFy2ryvuyB5Ne)",
         },
-        data: data
+        data: data,
     };
-    const response = ((await axios_1.default.request(config)).data).data;
+    const response = (await axios_1.default.request(config)).data.data;
     lastMusicId = response[response.length - 1].id;
     return {
         isEnd: pageNum > response.length,
-        data: response.map(formatMusicItem)
-    };
-}
-async function searchArtist(query, page) {
-    let data = JSON.stringify({
-        "searchType": "ARTIST",
-        "key": query,
-        "sinceId": 0,
-        "size": 2 * pageNum,
-    });
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://search-api.yinyuetai.com/search/get_search_result.json',
-        headers: {
-            'referrer': 'https://www.yinyuetai.com/',
-            'accept': 'application/json',
-            'content-type': 'application/json',
-            'wua': 'YYT/1.0.0 (WEB;web;11;zh-CN;kADiV2jNJFy2ryvuyB5Ne)'
-        },
-        data: data
-    };
-    const response = ((await axios_1.default.request(config)).data).data;
-    return {
-        isEnd: true,
-        data: response.map(formatArtistItem)
+        data: response.map(formatMusicItem),
     };
 }
 async function search(query, page, type) {
-    if (type === 'music') {
+    if (type === "music") {
         return await searchMusic(query, page);
-    }
-    else if (type === 'artist') {
-        return await searchArtist(query, page);
     }
 }
 async function getMediaSource(musicItem, quality) {
     let url;
-    if (quality === 'standard') {
-        url = musicItem.urls.find(it => it.streamType === 5).url;
+    if (quality === "standard") {
+        url = musicItem.urls.find((it) => it.streamType === 5).url;
     }
-    else if (quality === 'high') {
-        url = musicItem.urls.find(it => it.streamType === 1).url;
+    else if (quality === "high") {
+        url = musicItem.urls.find((it) => it.streamType === 1).url;
     }
     return {
-        url
+        url,
     };
-}
-let lastArtistId;
-async function getArtistWorks(artistItem, page, type) {
-    if (type === 'music') {
-        let sinceId = (page === 1 || artistItem.id !== lastArtistId) ? lastArtistId : 0;
-        const medias = (await axios_1.default.get("https://video-api.yinyuetai.com/video/listByArtist", {
-            "params": {
-                artistId: artistItem.id,
-                size: pageNum,
-                sinceId
-            }
-        })).data.data;
-        lastArtistId = medias[medias.length - 1].id;
-        return {
-            isEnd: medias.length < pageNum,
-            data: medias.map(formatMusicItem)
-        };
-    }
 }
 module.exports = {
     platform: "音悦台",
     version: "0.0.0",
-    supportedSearchType: ["music", "artist",],
+    supportedSearchType: ["music"],
     srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/yinyuetai/index.js",
     cacheControl: "no-cache",
     search,
     getMediaSource,
-    getArtistWorks,
 };
